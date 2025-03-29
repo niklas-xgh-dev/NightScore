b29682b9d2c62bb87f7bd53c85ee23b436838323
niklas-xgh-dev <techstag@outlook.com>
2025-03-29 16:07:14 +0100

    Working MVP that calculates a random number from 1 to 100 and displays it on the iphone

---
+++ NightScore/NightScore/NightScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightScoreApp.swift
+//  NightScore
+//
+//  Created by iroot on 29.03.25.
+//
+
+import SwiftUI
+
+@main
+struct NightScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NightScore/NightScoreTests/NightScoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  NightScoreTests.swift
+//  NightScoreTests
+//
+//  Created by iroot on 29.03.25.
+//
+
+import Testing
+@testable import NightScore
+
+struct NightScoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
